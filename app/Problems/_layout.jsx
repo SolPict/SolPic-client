b98490f8ee3aae58b6ab806a70dfe3bf11ae0c07
Yangsubo <sheeep555@gmail.com>
2024-09-27 14:18:49 +0900

    Refactor: hide tap headers

--- app/Problems/_layout.jsx
+++ app/Problems/_layout.jsx
@@ -3,8 +3,14 @@ import { Stack } from "expo-router";
 export default function ProblemLayout() {
   return (
     <Stack>
-      <Stack.Screen name="Problems" options={{ headerTitle: "문제풀기" }} />
-      <Stack.Screen name="[problemId]" options={{ headerTitle: "풀어보기" }} />
+      <Stack.Screen
+        name="Problems"
+        options={{ headerTitle: "문제풀기", headerShown: false }}
+      />
+      <Stack.Screen
+        name="[problemId]"
+        options={{ headerTitle: "풀어보기", headerShown: false }}
+      />
     </Stack>
   );
 }
