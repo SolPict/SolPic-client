db4718c747e6f5639da15191b4b07f1a63338a72
Yangsubo <sheeep555@gmail.com>
2024-09-27 14:29:12 +0900

    Feat: Create login, logout button

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,38 @@
-import { Tabs } from "expo-router";
+import { router, Tabs } from "expo-router";
 import TabBar from "../components/TabBar";
+import { MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
+import useClientStore from "../store/store";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../auth/firebaseConfig";
+import { useEffect } from "react";
 
 export default function TabLayout() {
+  const { setClientStatus, getClientStatus } = useClientStore();
+  const { isLogin } = getClientStatus();
+
+  const handleLoginAndLogout = () => {
+    if (isLogin) {
+      signOut(auth);
+      setClientStatus({ isLogin: true });
+    } else {
+      router.push("/Login");
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setClientStatus({ isLogin: true, email: currentUser.email });
+      } else {
+        setClientStatus({ isLogin: false, email: null });
+      }
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
   return (
     <Tabs tabBar={(props) => <TabBar {...props} />}>
       <Tabs.Screen
@@ -16,19 +47,60 @@ export default function TabLayout() {
         options={{
           title: "문제 풀이",
           headerShown: false,
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={handleLoginAndLogout}
+              style={styles.logoutContainer}
+            >
+              <MaterialIcons
+                style={styles.logoutButton}
+                name={isLogin ? "logout" : "login"}
+                size={24}
+                color="black"
+              />
+              <Text>{isLogin ? "로그아웃" : "로그인"}</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
       <Tabs.Screen
         name="index"
         options={{
           title: "메인으로",
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={handleLoginAndLogout}
+              style={styles.logoutContainer}
+            >
+              <MaterialIcons
+                style={styles.logoutButton}
+                name={isLogin ? "logout" : "login"}
+                size={24}
+                color="black"
+              />
+              <Text>{isLogin ? "로그아웃" : "로그인"}</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
       <Tabs.Screen
         name="Problems"
         options={{
           title: "문제풀기",
-          headerShown: false,
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={handleLoginAndLogout}
+              style={styles.logoutContainer}
+            >
+              <MaterialIcons
+                style={styles.logoutButton}
+                name={isLogin ? "logout" : "login"}
+                size={24}
+                color="black"
+              />
+              <Text>{isLogin ? "로그아웃" : "로그인"}</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
       <Tabs.Screen
@@ -42,15 +114,49 @@ export default function TabLayout() {
         name="ProblemReviews"
         options={{
           title: "리뷰노트",
-          headerShown: false,
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={handleLoginAndLogout}
+              style={styles.logoutContainer}
+            >
+              <MaterialIcons
+                style={styles.logoutButton}
+                name={isLogin ? "logout" : "login"}
+                size={24}
+                color="black"
+              />
+              <Text>{isLogin ? "로그아웃" : "로그인"}</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
       <Tabs.Screen
         name="PastHistory"
         options={{
           title: "과거기록",
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={handleLoginAndLogout}
+              style={styles.logoutContainer}
+            >
+              <MaterialIcons
+                style={styles.logoutButton}
+                name={isLogin ? "logout" : "login"}
+                size={24}
+                color="black"
+              />
+              <Text>{isLogin ? "로그아웃" : "로그인"}</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  logoutContainer: {
+    alignItems: "center",
+    right: 10,
+  },
+});
