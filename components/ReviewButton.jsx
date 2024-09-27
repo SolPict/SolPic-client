8d1a9a3ac53f3ad11d10ab7320a75802803cb3c6
Yangsubo <sheeep555@gmail.com>
2024-09-27 14:37:54 +0900

    Feat: Create ReviewButton component

---
+++ components/ReviewButton.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { router } from "expo-router";
+import { Alert, StyleSheet, Text, TouchableOpacity } from "react-native";
+import useClientStore from "../store/store";
+
+export default function ReviewButton({ problemId, chosenAnswer = 0 }) {
+  const { getClientStatus } = useClientStore();
+  const { email } = getClientStatus();
+
+  const addReviewNote = async () => {
+    try {
+      const result = await axios.post(
+        process.env.EXPO_PUBLIC_SERVER_URL + "problems/reviewNote/" + problemId,
+        {
+          email,
+          chosenAnswer,
+        }
+      );
+
+      router.push("/ProblemReviews/ReviewNote");
+    } catch (error) {
+      Alert.alert("리뷰노트 추가하는데 문제가 발생하였습니다.");
+      console.error("리뷰노트 추가하는데 문제가 발생하였습니다.", error);
+    }
+  };
+
+  return (
+    <TouchableOpacity style={styles.Button} onPress={addReviewNote}>
+      <Text style={styles.buttonText}>복습노트 추가</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  Button: {
+    backgroundColor: "white",
+    width: 180,
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    shadowColor: "black",
+    shadowRadius: 10,
+    shadowOpacity: 0.1,
+    top: 80,
+  },
+  buttonText: {
+    fontSize: 22,
+  },
+});
