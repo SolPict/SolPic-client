8e4097517aa2145ccfbbbf3a2cdec5608ff81b3e
Yangsubo <sheeep555@gmail.com>
2024-09-27 14:39:49 +0900

    Feat: Get review problem from server

--- app/ProblemReviews/ReviewNote.jsx
+++ app/ProblemReviews/ReviewNote.jsx
@@ -1,13 +1,48 @@
 import {
+  Alert,
+  Image,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
-import ColorList from "../../components/ProblemContainer";
+import useClientStore from "../../store/store";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { router, useFocusEffect } from "expo-router";
 
 export default function ReviewNote() {
+  const [ReviewNote, setReviewNote] = useState([]);
+  const { getClientStatus } = useClientStore();
+  const { email } = getClientStatus();
+
+  useFocusEffect(
+    useCallback(() => {
+      getReviewNote();
+    }, [])
+  );
+
+  const getReviewNote = async () => {
+    try {
+      const { data } = await axios.post(
+        process.env.EXPO_PUBLIC_SERVER_URL + "problems/reviewNote",
+        {
+          email,
+        }
+      );
+
+      setReviewNote(JSON.parse(data));
+    } catch (error) {
+      Alert.alert("리뷰노트 데이터를 가져오는데 실패하였습니다.", error);
+      console.error(
+        "리뷰노트 데이터를 가져오는데 실패하였습니다.",
+        Object.values(error)
+      );
+    }
+  };
+
   return (
     <SafeAreaView>
       <View style={styles.reviewHeader}>
@@ -16,7 +51,28 @@ export default function ReviewNote() {
           <Text style={styles.recommendText}>문제 추천</Text>
         </TouchableOpacity>
       </View>
-      <ColorList color="#4f3fe5" currentPage="ReviewNote" />
+      <ScrollView style={styles.container}>
+        {!ReviewNote.length ||
+          ReviewNote.map((problem) => {
+            return (
+              <TouchableOpacity
+                key={problem._id.$oid}
+                style={styles.problemContainer}
+                onPress={() =>
+                  router.push(
+                    `/ProblemReviews/${problem._id.$oid}?problem=` +
+                      encodeURIComponent(JSON.stringify(problem))
+                  )
+                }
+              >
+                <Image
+                  style={styles.problemImage}
+                  source={{ uri: problem.uri }}
+                />
+              </TouchableOpacity>
+            );
+          })}
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -42,5 +98,32 @@ const styles = StyleSheet.create({
   recommendText: {
     fontSize: 20,
     color: "white",
+  },
+  container: {
+    backgroundColor: "blue",
+    gap: 10,
+  },
+  problemContainer: {
+    backgroundColor: "white",
+    margin: 10,
+    borderRadius: 20,
+  },
+  problemHeader: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginHorizontal: 10,
+  },
+  headerText: {
+    fontSize: 17,
+  },
+  problemImage: {
+    width: "100%",
+    height: 200,
+    borderRadius: 20,
+  },
+  container: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    zIndex: 0,
   },
 });
