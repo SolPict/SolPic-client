49022dbd86aa3dc6e88672803c5e59414eba5d7c
Yangsubo <sheeep555@gmail.com>
2024-10-07 19:40:27 +0900

    Feat: Add delete modal

--- app/Problems/[problemId].jsx
+++ app/Problems/[problemId].jsx
@@ -5,17 +5,35 @@ import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 import axios from "axios";
 import useClientStore from "../../store/store";
+import { COLORS } from "../../constants/colors";
+import DeleteModal from "../../components/DeleteModal";
 
 export default function ProblemPage() {
+  const [modalVisible, setModalVisible] = useState(false);
   const { problemId, problem } = useLocalSearchParams();
   const problemInfo = JSON.parse(decodeURIComponent(problem));
   const [selectedRadio, setSelectedRadio] = useState(1);
   const { getClientStatus } = useClientStore();
   const { email } = getClientStatus();
+  const isUserAnswerCorrect = selectedRadio === problemInfo.answer;
+
+  const deleteReviewNote = () => {
+    try {
+      setModalVisible(true);
+
+      axios.delete(
+        process.env.EXPO_PUBLIC_SERVER_URL + "problem/solving/" + problemId,
+        {
+          email: email || "",
+        }
+      );
+    } catch {
+      console.error(error);
+    }
+  };
 
   const handleSubmit = () => {
     try {
-      const isUserAnswerCorrect = selectedRadio === problemInfo.answer;
       axios.post(
         process.env.EXPO_PUBLIC_SERVER_URL + "problem/solving/" + problemId,
         {
@@ -35,41 +53,88 @@ export default function ProblemPage() {
   };
 
   return (
-    <View>
+    <View style={styles.problemContainer}>
       <Image source={{ uri: problemInfo.uri }} style={styles.problemImage} />
+      <Text style={styles.radioTitle}>번호</Text>
       <RadioButton
         selectedRadio={selectedRadio}
         setSelectedRadio={setSelectedRadio}
       />
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity
+          style={styles.deleteContainer}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.deleteText}>삭제</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.submitContainer} onPress={handleSubmit}>
           <Text style={styles.submitText}>제출하기</Text>
         </TouchableOpacity>
       </View>
+      <DeleteModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        deleteReviewNote={deleteReviewNote}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  problemImage: {
+  problemContainer: {
     width: "100%",
-    height: 300,
-    resizeMode: "contain",
+    height: "100%",
+    backgroundColor: "white",
+    justifyContent: "centers",
   },
-  buttonContainer: {
-    alignItems: "flex-end",
-  },
-  submitButton: {
-    backgroundColor: "rgb(97 231 228)",
-    width: 100,
-    height: 50,
+  backButton: {
+    width: "10%",
+    height: "5%",
     justifyContent: "center",
     alignItems: "center",
+    left: 20,
+  },
+  problemImage: {
+    width: "90%",
+    height: "30%",
+    resizeMode: "contain",
+    margin: 20,
+  },
+  radioTitle: {
+    fontWeight: "800",
+    fontSize: 20,
+    margin: 20,
+  },
+  bottomContainer: {
+    width: "90%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+    bottom: 50,
+    gap: 20,
+  },
+  deleteContainer: {
+    backgroundColor: COLORS.PRIMARY,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "20%",
+    height: 50,
     borderRadius: 10,
-    margin: 10,
+  },
+  submitContainer: {
+    backgroundColor: COLORS.PRIMARY,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "73%",
+    height: 50,
+    borderRadius: 10,
+  },
+  deleteText: {
+    color: "white",
+    fontWeight: "700",
   },
   submitText: {
-    fontSize: 20,
-    fontWeight: "500",
+    color: "white",
+    fontWeight: "700",
   },
 });
