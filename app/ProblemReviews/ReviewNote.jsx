8be4ab5f8f59fef492d3eb43d22d0f374f51dbfa
Yangsubo <sheeep555@gmail.com>
2024-10-07 22:13:22 +0900

    Refactor: Add problemList component and rendering pages based on login status

--- app/ProblemReviews/ReviewNote.jsx
+++ app/ProblemReviews/ReviewNote.jsx
@@ -1,28 +1,26 @@
-import {
-  Alert,
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Alert, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import useClientStore from "../../store/store";
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
-import { router, useFocusEffect } from "expo-router";
+import { useFocusEffect } from "expo-router";
+import ProblemList from "../../components/ProblemList";
 
 export default function ReviewNote() {
   const [ReviewNote, setReviewNote] = useState([]);
   const { getClientStatus } = useClientStore();
-  const { email } = getClientStatus();
+  const { email, isLogin } = getClientStatus();
 
   useFocusEffect(
     useCallback(() => {
-      getReviewNote();
-    }, [])
+      if (isLogin) {
+        getReviewNote();
+      }
+    }, [email, isLogin])
   );
+
+  useEffect(() => {
+    getReviewNote();
+  }, [email, isLogin]);
 
   const getReviewNote = async () => {
     try {
@@ -40,87 +38,31 @@ export default function ReviewNote() {
     }
   };
 
-  return (
-    <SafeAreaView>
-      <View style={styles.reviewHeader}>
-        <Text style={styles.totalProblem}>총: 2문제</Text>
-        <TouchableOpacity activeOpacity={0.6} style={styles.recommendButton}>
-          <Text style={styles.recommendText}>문제 추천</Text>
-        </TouchableOpacity>
+  if (!isLogin) {
+    return (
+      <View style={styles.notLogInPage}>
+        <Text>해당 페이지는 로그인이 필요한 페이지입니다.</Text>
       </View>
-      <ScrollView style={styles.container}>
-        {!ReviewNote.length ||
-          ReviewNote.map((problem) => {
-            return (
-              <TouchableOpacity
-                key={problem._id.$oid}
-                style={styles.problemContainer}
-                onPress={() =>
-                  router.push(
-                    `/ProblemReviews/${problem._id.$oid}?problem=` +
-                      encodeURIComponent(JSON.stringify(problem))
-                  )
-                }
-              >
-                <Image
-                  style={styles.problemImage}
-                  source={{ uri: problem.uri }}
-                />
-              </TouchableOpacity>
-            );
-          })}
-      </ScrollView>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.reviewContainer}>
+      <ProblemList problems={ReviewNote} prevPage="reviewNote"></ProblemList>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  reviewHeader: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+  reviewContainer: {
+    top: 50,
+    height: "90%",
+    paddingVertical: 40,
   },
-  totalProblem: {
-    fontSize: 20,
-    fontWeight: "500",
-    margin: 10,
-    justifyContent: "space-evenly",
-  },
-  recommendButton: {
-    backgroundColor: "blue",
-    margin: 10,
-    padding: 5,
-    borderRadius: 10,
-  },
-  recommendText: {
-    fontSize: 20,
-    color: "white",
-  },
-  container: {
-    backgroundColor: "blue",
-    gap: 10,
-  },
-  problemContainer: {
-    backgroundColor: "white",
-    margin: 10,
-    borderRadius: 20,
-  },
-  problemHeader: {
-    flexDirection: "row",
-    marginTop: 10,
-    marginHorizontal: 10,
-  },
-  headerText: {
-    fontSize: 17,
-  },
-  problemImage: {
+  notLogInPage: {
     width: "100%",
-    height: 200,
-    borderRadius: 20,
-  },
-  container: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    zIndex: 0,
+    height: "90%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
