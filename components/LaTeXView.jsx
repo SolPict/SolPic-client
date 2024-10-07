476e28d1a318460c81d084a3e5381d9d00376957
Yangsubo <sheeep555@gmail.com>
2024-10-07 22:01:19 +0900

    Refactor: refactor LaTeXView component

--- components/LaTeXView.jsx
+++ components/LaTeXView.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import MathView from "react-native-math-view";
 
-export default function LaTeXView({ children }) {
+export default function LaTeXView({ problemId, children }) {
+  children = children.replace(/ +/g, " ").replaceAll("\\boxed", "$\\boxed");
+
   return (
     <View style={styles.container}>
-      {children.split("$").map((sentence) => {
+      {children.split("$").map((sentence, index) => {
         const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
         return korean.test(sentence) ? (
-          <Text>{sentence.trim().replace(/ +/g, " ")}</Text>
+          <Text style={styles.korText} key={problemId + index}>
+            {sentence.trim()}
+          </Text>
         ) : (
-          <MathView math={sentence} style={styles.mathView} />
+          <MathView key={problemId + index} math={sentence} />
         );
       })}
     </View>
@@ -21,11 +25,11 @@ export default function LaTeXView({ children }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     gap: 10,
+    margin: 10,
   },
-  mathView: {
+  korText: {
     width: "100%",
-    height: 100,
   },
+  mathText: {},
 });
