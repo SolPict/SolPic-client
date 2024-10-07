2be1c0552c3b332f4b9e9dcebe4b4584bbcf720f
Yangsubo <sheeep555@gmail.com>
2024-10-07 19:49:27 +0900

    Refactor: Change camera bottom and Add gallery button

---
+++ components/CameraBottom.jsx
@@ -0,0 +1,62 @@
+import { Image, StyleSheet, View } from "react-native";
+import { TouchableOpacity } from "react-native";
+import cameraImage from "../assets/photo_shot.png";
+import CameraBottomGallery from "./CameraBottomGallery";
+
+export default function CameraBottom({ setImage, cameraRef }) {
+  const takePhoto = async () => {
+    if (!cameraRef.current) {
+      return;
+    }
+
+    try {
+      const takedPhoto = await cameraRef.current.takePictureAsync();
+
+      setImage(JSON.stringify(takedPhoto));
+    } catch (error) {
+      console.error("사진찍는중 에러가 발생하였습니다.", error);
+    }
+  };
+
+  return (
+    <View style={styles.bottomContainer}>
+      <CameraBottomGallery />
+      <TouchableOpacity style={styles.buttonOuter} onPress={takePhoto}>
+        <View style={styles.buttonInner}>
+          <Image style={styles.photoShotImage} source={cameraImage} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bottomContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonOuter: {
+    position: "absolute",
+    bottom: 77,
+    backgroundColor: "black",
+    width: 67,
+    height: 67,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 40,
+  },
+  buttonInner: {
+    backgroundColor: "white",
+    width: 55,
+    height: 55,
+    borderRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  photoShotImage: {
+    position: "absolute",
+    resizeMode: "cover",
+    width: 20,
+    height: 20,
+  },
+});
---
+++ components/CameraBottomGallery.jsx
@@ -0,0 +1,91 @@
+import { router, useFocusEffect } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import { Alert, Image } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import * as ImagePicker from "expo-image-picker";
+
+export default function CameraBottomGallery() {
+  const [firstImage, setFirstImage] = useState(null);
+  const [permissionGranted, setPermissionGranted] = useState(false);
+
+  useEffect(() => {
+    const getPermission = async () => {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+
+      if (status === "granted") {
+        setPermissionGranted(true);
+      } else {
+        Alert.alert("갤러리 접근 권한이 필요합니다.");
+      }
+    };
+
+    getPermission();
+  }, []);
+
+  useEffect(() => {
+    if (permissionGranted) {
+      getFirstImage();
+    }
+  }, [permissionGranted]);
+
+  const getFirstImage = async () => {
+    if (!permissionGranted) {
+      Alert.alert("권한이 없습니다.");
+      return;
+    }
+
+    const media = await MediaLibrary.getAssetsAsync({
+      mediaType: "photo",
+      first: 1,
+      sortBy: MediaLibrary.SortBy.creationTime,
+    });
+
+    if (media.assets.length > 0) {
+      const asset = media.assets[0];
+      const assetInfo = await MediaLibrary.getAssetInfoAsync(asset.id);
+
+      setFirstImage(assetInfo.localUri);
+    } else {
+      Alert.alert("갤러리에 이미지가 없습니다.");
+    }
+  };
+
+  const uploadImage = async () => {
+    const { assets } = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      aspect: [4, 3],
+      quality: 1,
+      allowsEditing: true,
+    });
+
+    router.push(
+      "/AnalyzingProblem?image=" + encodeURIComponent(JSON.stringify(assets[0]))
+    );
+  };
+
+  return (
+    <TouchableOpacity style={styles.gallery} onPress={uploadImage}>
+      {firstImage && (
+        <Image style={styles.firstImage} source={{ uri: firstImage }} />
+      )}
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  gallery: {
+    position: "absolute",
+    width: 35,
+    height: 35,
+    right: 110,
+    bottom: 90,
+    borderRadius: 10,
+  },
+  firstImage: {
+    resizeMode: "cover",
+    width: 35,
+    height: 35,
+    borderRadius: 10,
+  },
+});
